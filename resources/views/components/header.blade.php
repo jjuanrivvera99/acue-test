5d56c7e72a06b24b4a6c8f0f116c4ee5ff518c22
Juan Felipe Rivera G <juan.rivera@alegra.com>
2023-01-10 13:36:20 -0500

    Mobile view logo updated

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -15,8 +15,8 @@
     <!--end::Sidebar mobile toggle-->
     <!--begin::Mobile logo-->
     <div class="d-flex align-items-center flex-grow-1 flex-lg-grow-0">
-        <a href="../../index.html" class="d-lg-none">
-            <img alt="Logo" src="assets/media/logos/default-small.svg" class="h-30px" />
+        <a href="/" class="d-lg-none">
+            <img alt="Logo" src="assets/media/logos/acue-logo-small.png" class="h-30px" />
         </a>
     </div>
     <!--end::Mobile logo-->
